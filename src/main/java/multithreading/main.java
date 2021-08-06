1ed13cc84ac82421a00f828c4fc4172282ec62f1
Dilara  polat <polatdilarasevim@gmail.com>
2021-08-06 21:09:08 +0300

    kodluyoruz multithreading

--- src/main/java/multithreading/main.java
+++ src/main/java/multithreading/main.java
@@ -1,30 +1,30 @@
 package multithreading;
 
-public class main {
-	 public static void main( String[] args )
-	    {
-	    	
-	      System.out.println("Main thread is starting");
-	          
-	          app App = new app("thread");
-	          Thread thread = new Thread(App);
-	        
-	          thread.start();
-	          int i;
-	          for ( i = 0; i < 20; i++) {
-	              System.out.print("-");
-	              try {
-	                  Thread.sleep(100);
-	              } catch (InterruptedException e) {
-	                  System.out.println(" Thread kesintiye ugradi.!");
-	              }
-	          }
-	          System.out.println(" Thread sonlandý:)");
-	    	
-	    	
-	      
+public class main implements Runnable {
 
-	    	
-	      }
+	 public static void main(String[] args) {
 
-}
+	  Thread thread = new Thread();
+	  thread.start();
+	  try {
+
+	   int i;
+	   for (i = 1; i < 16; i++) {
+
+	    Thread.sleep(400);
+	    thread.setName(i + ". string");
+	    System.out.println(thread.getName());
+
+	   }
+	  } catch (InterruptedException e) {
+	   System.out.println(e);
+	  }
+
+	 }
+
+	 public void run() {
+	  // TODO Auto-generated method stub
+	 }
+
+	}
+
